da3c5602a97ef693125039e2a1bbb431ca9ce8f0
Jefferson Ferreira <x21223467@ncirl.ie>
2026-03-31 01:04:11 +0100

    Add NamingServer main class

---
+++ src/main/java/ie/nci/smartbuilding/naming/NamingServer.java
@@ -0,0 +1,35 @@
+/*
+ * Student: Jefferson Ferreira
+ * ID: 21223467
+ * Subject: Distributed Systems
+ * Lecturer: Catriona Nic Lughadha
+ * National College of Ireland
+ CA - Climate Action
+ Java files
+ */
+
+package ie.nci.smartbuilding.naming;
+
+import.io.grpc.Server;
+import.io.grpc.ServerBuilder;
+
+public class NamingServer {
+
+    public static final int PORT = 50050;
+
+    public static void main(String[]args) throws Exception{
+        Server server = ServerBuilder.forPort(PORT)
+                .addService(new NamingServiceImpl())
+                .build()
+                .start();
+
+        System.out.println("[NamingServer] Started on port" + PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("[NamingServer] Shutting down...");
+            server.shutdown();
+        }));
+
+        server.awaitTermination();
+    }
+}
